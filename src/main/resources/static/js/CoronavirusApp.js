8f5447b023f689c8052ee32196519d4bbbbd3c88
Diego Puerto <diego23p@hotmail.com>
2020-03-25 17:15:01 -0500

    sumando

--- src/main/resources/static/js/CoronavirusApp.js
+++ src/main/resources/static/js/CoronavirusApp.js
@@ -30,13 +30,36 @@ var CoronavirusApp =( function (){
 
 		countries=JSON.parse(countries);
 		countries = countries.data.covid19Stats;
-		
+		var muertes = new Map();
+		var confirmado = new Map();
+		var sano = new Map();
+		var pais = [];
         var lista = countries.map(function(result){
+			if (muertes.get(result.country)==undefined){
+				pais.push(result.country);
+				muertes.set(result.country,result.deaths);
+				confirmado.set(result.country,result.confirmed);
+				sano.set(result.country,result.recovered);
+			} else{
+				var nuevo = muertes.get(result.country) + result.deaths;
+				muertes.set(result.country,nuevo);
+				
+				var nuevo2 = confirmado.get(result.country) + result.confirmed;
+				confirmado.set(result.country,nuevo2);
+				
+				var nuevo3 = sano.get(result.country) + result.recovered;
+				sano.set(result.country,nuevo3);
+			}
 			return {country:result.country, deaths:result.deaths, confirmed:result.confirmed, recovered:result.recovered}
 		})
-			var contenedor = $("<tr><td class=\"lalign\">"+result.country+"</td><td>"+result.deaths+"</td><td>"+result.confirmed+"</td><td>"+result.recovered+"</td><td><button class='btn btn-primary'>Ver</button></td></tr>");
+		//alert(pais);
+		//alert(muertes.get("China"));
+		//alert(confirmado.get("China"));
+		//alert(sano.get("China"));
+		pais.map(function(result){
+			var contenedor = $("<tr><td class=\"lalign\">"+result+"</td><td>"+muertes.get(result)+"</td><td>"+confirmado.get(result)+"</td><td>"+sano.get(result)+"</td><td><button class='btn btn-primary'>Ver</button></td></tr>");
 			contenedor.click(function(){
-				pintar(result.country);
+				pintar(result);
 			});
 			$("#keywords tbody").append(contenedor);
 		})
